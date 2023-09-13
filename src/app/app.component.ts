e11b6535b2fd86dcf147303f106e83e2d0d9c73b
Oluwatobiloba Adebayo <hardebaryorone@gmail.com>
2023-09-13 11:04:56 +0100

    update secure id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'secure-id-localize';
+  title = $localize`secure-id-localize`;
+  value = window.location.href.includes('ar-AE') ? 'ar' : 'en';
+
+  onChange(event: any) {
+    let value = 'en-US';
+    if (event.target.value === 'ar') {
+      value = 'ar-AE';
+    }
+
+    window.open(`${location.origin}/${value}/`, '_self');
+  }
 }
